6b5dc09bf18d8995109d3bdad7c104f9f647774a
impu1se <impu1se@bk.ru>
2016-10-11 17:51:05 +0300

    half

--- lang_frequency.py
+++ lang_frequency.py
@@ -1,12 +1,31 @@
+import collections
+import argparse
+import sys
+
+
+parser = argparse.ArgumentParser(description='Pretty print for JSON')
+parser.add_argument('-file', dest='filepath',
+                    help='Input filepath and filename with expansion json' )
+args = parser.parse_args()
 
 
 def load_data(filepath):
-    pass
+    try:
+        with open(filepath, encoding='utf-8') as f:
+            return f.read()
+    except (FileNotFoundError):
+        print('Файл не найден')
+        sys.exit(1)
 
 
 def get_most_frequent_words(text):
-    pass
+    count_word = collections.Counter()
+    for word in text.split():
+        count_word[word] += 1
+    return count_word
+
 
 
 if __name__ == '__main__':
-    pass
+    data = load_data(args.filepath)
+    print(get_most_frequent_words(data))
