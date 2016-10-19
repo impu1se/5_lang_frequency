70df5359f45b07a47494babf2a9a5555b72482c9
impu1se <impu1se@bk.ru>
2016-10-19 12:29:24 +0300

    finally3

--- lang_frequency.py
+++ lang_frequency.py
@@ -5,31 +5,27 @@ import re
 from string import punctuation
 
 
-parser = argparse.ArgumentParser(description='Top 10 word in text')
-parser.add_argument('-file', dest='filepath',
-                    help='Input filepath and filename')
-args = parser.parse_args()
-
-
 def load_data(filepath):
-    try:
-        with open(filepath, encoding='utf-8') as f:
+    with open(filepath, encoding='utf-8') as f:
             return f.read()
-    except FileNotFoundError:
-        print('Файл не найден')
-        sys.exit(1)
-    except ValueError:
-        print('Формат файла не подходит для вывода данных')
 
 
 def get_most_frequent_words(text):
     count_word = collections.Counter()
-    words = re.sub(r'[{}]'.format(punctuation), "", text)
-    for word in words.split(' '):
+    words = re.sub(r'[{}{}]'.format(punctuation, '\\n\\t\\r'), " ", text)
+    for word in words.split():
         count_word[word] += 1
     return sorted(count_word.items(), key=lambda x: x[1], reverse=True)[:10]
 
 
 if __name__ == '__main__':
-    data = load_data(args.filepath)
+    parser = argparse.ArgumentParser(description='Top 10 word in text')
+    parser.add_argument('-file', dest='filepath',
+                        help='Input filepath and filename')
+    args = parser.parse_args()
+    try:
+        data = load_data(args.filepath)
+    except FileNotFoundError:
+        print('File not Found')
+        sys.exit(1)
     print(get_most_frequent_words(data))
